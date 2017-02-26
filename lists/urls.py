e7482e7da5b24082ed47f419dff700237cb32fd5
redshiftzero <jen@redshiftzero.com>
2017-02-26 00:00:04 -0800

    Refactor hard-coded URLs out of templates

--- lists/urls.py
+++ lists/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls import include, url
 from lists import views
 
 urlpatterns = [
-    url(r'^new$', 'lists.views.new_list', name='new_list'),
-    url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^new$', views.new_list, name='new_list'),
+    url(r'^(\d+)/$', views.view_list, name='view_list'),
 ]
