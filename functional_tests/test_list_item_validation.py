1d28f531f1dde5e3ad68da16aacfb1ca995f5a3c
redshiftzero <jen@redshiftzero.com>
2017-02-25 21:39:52 -0800

    Adjust new list view to do model validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -10,7 +10,7 @@ class ItemValidationTest(FunctionalTest):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.RETURN)
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank.
@@ -18,11 +18,13 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # She tries again with some text for the item, which now works.
-        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        form_field = self.browser.find_element_by_id('id_new_item')
+        form_field.send_keys('Buy milk')
+        form_field.send_keys(Keys.RETURN)
         self.check_for_row_in_list_table('1: Buy milk')
 
         # Perversely, she now decides to submit a second blank list item.
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.RETURN)
 
         # She receives a similar warning on the list page.
         self.check_for_row_in_list_table('1: Buy milk')
@@ -30,6 +32,8 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # And she can correct it by filling some text in
-        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        form_field = self.browser.find_element_by_id('id_new_item')
+        form_field.send_keys('Make tea')
+        form_field.send_keys(Keys.RETURN)
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
