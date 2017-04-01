1f83bcda86695864c8d1f9b9172eca6bef176bdf
redshiftzero <jen@redshiftzero.com>
2017-04-01 02:19:55 +0200

    Functional test for login via email

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 import sys
+import time
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+MAX_WAIT = 10
 
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -34,3 +38,13 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
+
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
