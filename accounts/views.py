e97f2cb3c53803fac836d5db0a52e8bd67575ca9
redshiftzero <jen@redshiftzero.com>
2017-04-04 09:52:11 -0700

    Add unit tests for messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 
@@ -6,8 +7,12 @@ def send_login_email(request):
     email = request.POST['email']
     send_mail(
         'Your login link for Superlists',
-        'body text tbc',
+        'Use this link to log in',
         'noreply@superlists',
         [email],
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
     )
     return redirect('/')
