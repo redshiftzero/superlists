38082a29b2b2355c8eaae5a994614a86a76e8d3d
redshiftzero <jen@redshiftzero.com>
2017-05-07 21:27:15 -0700

    Lists can have owners

--- lists/models.py
+++ lists/models.py
@@ -1,9 +1,12 @@
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
 
 # Create your models here.
 
 class List(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
 
